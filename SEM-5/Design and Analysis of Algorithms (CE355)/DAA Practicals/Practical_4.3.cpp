5b698d9a2018bd11f98b6227a775951c7493b8db
YatharthChuahan2362 <yc232326@gmail.com>
2022-09-08 07:37:57 +0530

    B.Tech-Computer-Engineering

---
+++ SEM-5/Design and Analysis of Algorithms (CE355)/DAA Practicals/Practical_4.3.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+struct Job
+{
+
+    char id;
+    int dead;
+    int profit;
+};
+
+bool comparison(Job a, Job b)
+{
+    return (a.profit > b.profit);
+}
+
+void printJobScheduling(Job arr[], int n)
+{
+
+    sort(arr, arr + n, comparison);
+
+    int result[n];
+    bool slot[n];
+
+    for (int i = 0; i < n; i++)
+        slot[i] = false;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = min(n, arr[i].dead) - 1; j >= 0; j--)
+        {
+
+            if (slot[j] == false)
+            {
+                result[j] = i;
+                slot[j] = true;
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        if (slot[i])
+            cout << arr[result[i]].id << " ";
+}
+
+int main()
+{
+    Job arr[] = {{'a', 2, 10},
+                 {'b', 1, 19},
+                 {'c', 2, 27},
+                 {'d', 3, 25},
+                 {'e', 4, 15}};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Following is maximum profit sequence of jobs: \n";
+    cout << "20DCE019-Yatharth Chuahan";
+
+    printJobScheduling(arr, n);
+    return 0;
+}
