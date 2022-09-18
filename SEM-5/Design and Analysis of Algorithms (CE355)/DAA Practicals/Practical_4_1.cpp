c66a9ba2a6d36c9d5207026d1989b6ad2ab321bd
YatharthChuahan2362 <yc232326@gmail.com>
2022-09-18 20:41:36 +0530

    B.Tech-Computer-Engineering

--- SEM-5/Design and Analysis of Algorithms (CE355)/DAA Practicals/Practical_4_1.cpp
+++ SEM-5/Design and Analysis of Algorithms (CE355)/DAA Practicals/Practical_4_1.cpp
@@ -1,89 +1,123 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-typedef struct
-{
-    double v;
-    double w;
-} Item;
-void input(Item items[], int sizeOfItems)
-{
-    cout << "Enter total " << sizeOfItems << " Item's values and weight" << endl;
-    for (int i = 0; i < sizeOfItems; i++)
-    {
-        cout << "Enter " << i + 1 << " V ";
-        cin >> items[i].v;
-        cout << "Enter " << i + 1 << " W ";
-        cin >> items[i].w;
-    }
-}
-void display(Item items[], int sizeOfItems)
-{
-    int i;
-    cout << "values: ";
-    for (i = 0; i < sizeOfItems; i++)
-    {
-        cout << items[i].v << "\t";
-    }
-    cout << endl
-         << "weight: ";
-    for (i = 0; i < sizeOfItems; i++)
-    {
-        cout << items[i].w << "\t";
-    }
-    cout << endl;
-}
-bool compare(Item a, Item b)
-{
-    double r1 = (double)(a.v / a.w);
-    double r2 = (double)(b.v / b.w);
-    return r1 > r2;
-}
-double knapsack(Item items[], int sizeOfItems, int W)
-{
-    int i, j;
-    double totalValue = 0, totalWeight = 0;
-    cout << "Profit per unit of weight :\n";
-    cout << "Value Weight Profit\n";
-    for (int i = 0; i < sizeOfItems; i++)
 
+// DSU data structure
+// path compression + rank by union
+
+class DSU
+{
+    int *parent;
+    int *rank;
+
+public:
+    DSU(int n)
     {
-        cout << items[i].v << " " << items[i].w << " "
-             << ((double)items[i].v / items[i].w) << endl;
+        parent = new int[n];
+        rank = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = -1;
+            rank[i] = 1;
+        }
     }
-    sort(items, items + sizeOfItems, compare);
-    for (i = 0; i < sizeOfItems; i++)
+
+    // Find function
+    int find(int i)
     {
-        if (totalWeight + items[i].w <= W)
+        if (parent[i] == -1)
+            return i;
+
+        return parent[i] = find(parent[i]);
+    }
+
+    // Union function
+    void unite(int x, int y)
+    {
+        int s1 = find(x);
+        int s2 = find(y);
+
+        if (s1 != s2)
         {
-            totalValue += items[i].v;
-            totalWeight += items[i].w;
-        }
-        else
-        {
-            int wt = W - totalWeight;
-            totalValue += items[i].v * ((double)wt / items[i].w);
-            totalWeight += wt;
-            break;
+            if (rank[s1] < rank[s2])
+            {
+                parent[s1] = s2;
+                rank[s2] += rank[s1];
+            }
+            else
+            {
+                parent[s2] = s1;
+                rank[s1] += rank[s2];
+            }
         }
     }
-    cout << "Total weight in bag " << totalWeight << endl;
-    return totalValue;
-}
+};
+
+class Graph
+{
+    vector<vector<int>> edgelist;
+    int V;
+
+public:
+    Graph(int V) { this->V = V; }
+
+    void addEdge(int x, int y, int w)
+    {
+        edgelist.push_back({w, x, y});
+    }
+
+    void kruskals_mst()
+    {
+        // 1. Sort all edges
+        sort(edgelist.begin(), edgelist.end());
+
+        // Initialize the DSU
+        DSU s(V);
+        int ans = 0;
+        cout << "Following are the edges in the "
+                "constructed MST"
+             << endl;
+        for (auto edge : edgelist)
+        {
+            int w = edge[0];
+            int x = edge[1];
+            int y = edge[2];
+
+            // Take this edge in MST if it does
+            // not forms a cycle
+            if (s.find(x) != s.find(y))
+            {
+                s.unite(x, y);
+                ans += w;
+                cout << x << " -- " << y << " == " << w
+                     << endl;
+            }
+        }
+
+        cout << "Minimum Cost Spanning Tree: " << ans;
+    }
+};
+
+// Driver's code
 int main()
 {
-    int W, n;
-    cout << "Enter total number of items :";
-    cin >> n;
-    Item items[n];
-    input(items, n);
-    cout << "Entered data \n";
-    display(items, n);
-    cout << "Enter Knapsack weight \n";
-    cin >> W;
-    double mxVal = knapsack(items, n, W);
-    cout << "Max Profit for " << W << " weight is : " << mxVal;
+    /* Let us create following weighted graph
+       10
+      0--------1
+      | \  |
+      6| 5\ |15
+      |  \ |
+      2--------3
+       4  */
+    Graph g(4);
+    g.addEdge(0, 1, 10);
+    g.addEdge(1, 3, 15);
+    g.addEdge(2, 3, 4);
+    g.addEdge(2, 0, 6);
+    g.addEdge(0, 3, 5);
 
+    // Function call
+    g.kruskals_mst();
     cout << "\n20DCE019-Yatharth Chauhan";
     return 0;
 }
