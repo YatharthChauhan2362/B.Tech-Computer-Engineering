50f39e15bc6a7985cd97f657eb391c0b15237b99
YatharthChuahan2362 <yc232326@gmail.com>
2022-10-16 00:04:02 +0530

    B.Tech-Computer-Engineering

--- SEM-5/Operating System (CE354)/OS Practicals/a.c
+++ SEM-5/Operating System (CE354)/OS Practicals/a.c
@@ -1,56 +1,56 @@
 #include <stdio.h>
-void main()
-{ // initlialize the variable name
-    int i, NOP, sum = 0, count = 0, y, quant, wt = 0, tat = 0, at[10], bt[10], temp[10];
-    float avg_wt, avg_tat;
-    printf(" Total number of process in the system: ");
-    scanf("%d", &NOP);
-    y = NOP; // Assign the number of process to variable y // Use for loop to enter the details of the process like Arrival time and the Burst Time
-    for (i = 0; i < NOP; i++)
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(int argc, char *argv[])
+{
+    int val[10], ele, status;
+    pid_t pid;
+    char *cval[10];
+    char *newenviron[] = {NULL};
+    int i, j, n, temp;
+    printf("\nEnter the size for an array: ");
+    scanf("%d", &n);
+    printf("\nEnter %d elements : ", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &val[i]);
+    printf("\nEntered elements are: ");
+    for (i = 0; i < n; i++)
+        printf("\t%d", val[i]);
+    for (i = 1; i < n; i++)
     {
-        printf("\n Enter the Arrival and Burst time of the Process[%d]\n", i + 1);
-        printf(" Arrival time is: \t"); // Accept arrival time scanf("%d", &at[i]); printf(" \nBurst time is: \t"); // Accept the Burst time
-        scanf("%d", &bt[i]);
-        temp[i] = bt[i]; // store the burst time in temp array } // Accept the Time qunat
-        printf("Enter the Time Quantum for the process: \t");
-        scanf("%d", &quant); // Display the process No, burst time, Turn Around Time and the waiting time
-        printf("\n Process No \t\t Burst Time \t\t TAT \t\t Waiting Time ");
-        for (sum = 0, i = 0; y != 0;)
+        for (j = 0; j < n - 1; j++)
         {
-            if (temp[i] <= quant && temp[i] > 0) // define the conditions
+            if (val[j] > val[j + 1])
             {
-                sum = sum + temp[i];
-                temp[i] = 0;
-                count = 1;
+                temp = val[j];
+                val[j] = val[j + 1];
+                val[j + 1] = temp;
             }
-            else if (temp[i] > 0)
-            {
-                temp[i] = temp[i] - quant;
-                sum = sum + quant;
-            }
-            if (temp[i] == 0 && count == 1)
-            {
-                y--; // decrement the process no.
-                printf("\nProcess No[%d] \t\t %d\t\t\t\t %d\t\t\t %d", i + 1, bt[i], sum - at[i], sum - at[i] - bt[i]);
-                wt = wt + sum - at[i] - bt[i];
-                tat = tat + sum - at[i];
-                count = 0;
-            }
-            if (i == NOP - 1)
-            {
-                i = 0;
-            }
-            else if (at[i + 1] <= sum)
-            {
-                i++;
-            }
-            else
-            {
-                i = 0;
-            }
-        } // represents the average waiting time and Turn Around time
-        avg_wt = wt * 1.0 / NOP;
-        avg_tat = tat * 1.0 / NOP;
-        printf("\n Average Turn Around Time: \t%f", avg_wt);
-        printf("\n Average Waiting Time: \t%f", avg_tat);
+        }
     }
+    printf("\nSorted elements are: ");
+    for (i = 0; i < n; i++)
+        printf("\t%d", val[i]);
+    wait(&status);
+    printf("\nparrent process complete");
+    printf("\nEnter element to search: ");
+    scanf("%d", &ele);
+    val[i] = ele;
+    for (i = 0; i < n + 1; i++)
+    {
+        char a[sizeof(int)];
+        snprintf(a, sizeof(int), "%d", val[i]);
+        cval[i] = malloc(sizeof(a));
+        strcpy(cval[i], a);
+    }
+    cval[i] = NULL;
+    pid = fork();
+    if (pid == 0)
+    {
+        execve(argv[1], cval, newenviron);
+        perror("Error in execve call...");
+    }
+}
