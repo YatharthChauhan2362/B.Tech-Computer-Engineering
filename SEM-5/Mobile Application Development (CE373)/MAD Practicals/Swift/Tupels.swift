17bda57e55a032d629e4c1f6cd56d7fc0b948c1e
YatharthChuahan2362 <yc232326@gmail.com>
2022-11-04 15:51:16 +0530

    B.Tech-Computer-Engineering

--- SEM-5/Mobile Application Development (CE373)/MAD Practicals/Swift/Tupels.swift
+++ SEM-5/Mobile Application Development (CE373)/MAD Practicals/Swift/Tupels.swift
@@ -4,3 +4,14 @@
 
 let fullname = ("Yatharth","Chauhan") //String, String
 print(fullname.1)
+
+
+let (first,last) = ("Ashish", "patel")
+print(first)
+
+
+let (f1, _) = ("as", "you")
+print(f1)
+
+ 
+
---
+++ SEM-5/Mobile Application Development (CE373)/MAD Practicals/Swift/tempCodeRunnerFile.swift
@@ -0,0 +1,4 @@
+let three = stride(from: 3, to: 10, by: 3)
+for n in three{
+    print(n)
+}
